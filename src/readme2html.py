8d7057697bd57da80f5ca101179aa47e85a57eab
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-07-18 23:14:31 +0530

    Updated requirments

--- src/readme2html.py
+++ src/readme2html.py
@@ -2,6 +2,7 @@ import markdown
 import pathlib
 from markdown.extensions.toc import TocExtension
 
+
 def getHtml(filepath: str) -> str:
     """Returns Html text from markdown
 
@@ -14,12 +15,14 @@ def getHtml(filepath: str) -> str:
 
     source = pathlib.Path(filepath).read_text()
     # return markdown.markdown(source)
-    return markdown.markdown(source, extensions=['fenced_code', 'codehilite','toc'])
+    return markdown.markdown(source, extensions=['fenced_code', 'codehilite', 'toc'])
 
-if __name__ == '__main__':    
+
+if __name__ == '__main__':
     md_text = '# Hello\n\n**Text**'
     html = markdown.markdown(md_text)
     print(html)
-    html = getHtml(r'C:\Users\Atharva\Desktop\Projects\project2portfolio\README.md')
-    with open('example.html','w') as f:
+    html = getHtml(
+        r'C:\Users\Atharva\Desktop\Projects\project2portfolio\README.md')
+    with open('example.html', 'w') as f:
         f.write(html)
