7ac7822d25551ec1a2dbf8e7d55c7fc578e3f059
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-07-19 16:46:43 +0530

    Feat : colaburl

--- src/metadataUtils.py
+++ src/metadataUtils.py
@@ -9,4 +9,15 @@ def geturl() -> str:
     """
     return subprocess.run('git config --get remote.origin.url'.split(" "),
                             stdout=subprocess.PIPE).stdout.decode('utf-8')
-                    
+
+def colaburl(url: str)-> str:
+    """Returns colab url for a given ipynb notebook hosted on github
+
+    Args:
+        url (str): ipynb github url
+
+    Returns:
+        str: colab url
+    """
+
+    return 'http://colab.research.google.com/github/' + url[19:]
